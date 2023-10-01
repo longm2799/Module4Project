2f0f5b75074d29ce21ca3caf71ecc133b5ff114a
longm2799 <144405458+longm2799@users.noreply.github.com>
2023-10-01 18:06:19 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,22 @@
-import requests
+import requests 
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    className = 'cnit 381'
+
+    return render_template('index.html', data=className)
+
+@app.route('/about')
+def about():
+    return 'About'
+
+@app.route('/another')
+def another():
+    return 'Another'
 
 url = "http://api.ipstack.com/check?access_key=ea37f7dd47cf2408fdaadd4003ae92a8"
 json_data = requests.get(url).json()
