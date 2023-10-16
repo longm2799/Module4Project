95a0db8b0b457fa3552f7220b1611e2594066ab2
longm2799 <144405458+longm2799@users.noreply.github.com>
2023-10-16 12:43:05 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,15 @@
 import requests 
 from flask import Flask, render_template
-class data:
-    url = "http://api.ipstack.com/check?access_key=ea37f7dd47cf2408fdaadd4003ae92a8"
-    json_data = requests.get(url).json()
-print("The Ip Address: "+ (data.json_data["ip"]))
+
+url = "http://api.ipstack.com/check?access_key=ea37f7dd47cf2408fdaadd4003ae92a8"
+json_data = requests.get(url).json()
+print("The Ip Address: "+ (json_data["ip"]))
 
 app = Flask(__name__)
 @app.route('/')
 def home():
 
-    className = (data.json_data["ip"])
+    className = (json_data["ip"])
 
     return render_template('index.html', data=className)
 
