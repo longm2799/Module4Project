d3483eb47c59efed935c6877c3221b8dc7e3641f
longm2799 <144405458+longm2799@users.noreply.github.com>
2023-10-16 12:10:37 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 import requests 
 from flask import Flask, render_template
-
+class data:
 url = "http://api.ipstack.com/check?access_key=ea37f7dd47cf2408fdaadd4003ae92a8"
 json_data = requests.get(url).json()
 print("The Ip Address: "+ (json_data["type"]))
