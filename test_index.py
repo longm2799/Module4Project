bc91b9642097055b62ec655f6ee04cb0bcb14996
longm2799 <144405458+longm2799@users.noreply.github.com>
2023-10-16 12:29:57 -0500

    Update test_index.py

--- test_index.py
+++ test_index.py
@@ -1,8 +1,9 @@
 import requests
+from api import data
 
 
 def test_classname():
- assert url == "http://api.ipstack.com/check?access_key=ea37f7dd47cf2408fdaadd4003ae92a8"
- assert json_data == requests.get(data.url).json()
+ assert data.url == "http://api.ipstack.com/check?access_key=ea37f7dd47cf2408fdaadd4003ae92a8"
+ assert data.json_data == requests.get(data.url).json()
  assert className == (data.json_data["ip"])
 
