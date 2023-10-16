369472965b6640d0b604dbe0ef7daaac53f1a0c0
longm2799 <144405458+longm2799@users.noreply.github.com>
2023-10-16 11:56:45 -0500

    Update test_index.py

--- test_index.py
+++ test_index.py
@@ -1,3 +1,7 @@
+import url 
+
 def test_classname():
+  url = "http://api.ipstack.com/check?access_key=ea37f7dd47cf2408fdaadd4003ae92a8"
+  json_data = requests.get(url).json()
   className = (json_data["ip"])
 
